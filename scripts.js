943c9babf5f1c512ff55e789a944bfa74ac3bb71
Adam <82294079+richter53@users.noreply.github.com>
2024-11-22 23:06:19 +0100

    mm

--- scripts.js
+++ scripts.js
@@ -176,35 +176,6 @@ const hangupCall = () => {
 };
 
 
-// Function to toggle mute/unmute
-function toggleMute() {
-    const videoElement = document.querySelector('#my-video');
-    if (videoElement.muted) {
-        videoElement.muted = false;
-    } else {
-        videoElement.muted = true;
-    }
-}
-
-// Function to toggle camera on/off
-function toggleCamera() {
-    const videoElement = document.querySelector('#my-video');
-    const stream = videoElement.srcObject;
-    const tracks = stream.getTracks();
-
-    tracks.forEach(track => {
-        if (track.kind === 'video') {
-            track.enabled = !track.enabled; // Toggle the camera
-        }
-    });
-}
-
-
-
-
 document.querySelector('#call').addEventListener('click',call)
 
-document.querySelector('#hangup').addEventListener('click', () => {
-    console.log("Hangup button clicked"); // Debug log
-    hangupCall();
-});
+document.querySelector('#hangup').addEventListener('click', hangupCall);
